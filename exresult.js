a47a87db952f6b2f32059bd81920abee33d94dbb
matcha20070516 <ma0516_hogreen@icloud.com>
2025-09-29 08:48:28 +0900

    Update exresult.js

--- exresult.js
+++ exresult.js
@@ -17,7 +17,27 @@ if (reviewBtn) {
   reviewBtn.addEventListener("click", () => {
     localStorage.setItem("exReviewMode", "true");
     localStorage.setItem("exCurrent", "1"); // 1問目から開始
-    window.location.href = "exproblem_set1.html"; // ←問題ページ
+
+    // 現在のセットを取得
+    const setName = localStorage.getItem("exSetName");
+
+    let targetPage = "";
+    switch (setName) {
+      case "謎検模試_M":
+        targetPage = "exproblem_set1.html";
+        break;
+      case "謎検模試test":
+        targetPage = "exproblem_set2.html";
+        break;
+      case "謎検模試_set3":
+        targetPage = "exproblem_set3.html";
+        break;
+      default:
+        targetPage = "exproblem_set1.html"; // fallback
+        break;
+    }
+
+    window.location.href = targetPage;
   });
 }
 
