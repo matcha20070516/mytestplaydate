ad8cd9460e8e6cdb6b4f37d69921cce7b036399e
matcha20070516 <ma0516_hogreen@icloud.com>
2025-10-06 12:59:02 +0900

    Update exproblem_set1.js

--- exproblem_set1.js
+++ exproblem_set1.js
@@ -216,7 +216,7 @@ const handleExamEnd = (message) => {
     localStorage.getItem("exUsername") ||
     "名無し";
 
-  const setName = localStorage.getItem("exSetName") || "謎検模試_M";
+  const setName = "謎検模試_M"; // set1専用の模試名
   const score = calculateScore(answers);
   const grade = getGrade(score);
 
